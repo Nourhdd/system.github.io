fd10c16a177eb4c6d3fd0df41bcd5603d7fa628f
Nourhdd <147275624+Nourhdd@users.noreply.github.com>
2024-10-19 20:42:10 +0200

    Delete myproject directory

--- myproject/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include  # Assure-toi que 'include' est importé
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('myapp.urls')),  # Utilisation de 'include' pour inclure les urls de l'application
-]
